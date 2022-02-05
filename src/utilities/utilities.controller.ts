43ba1a8a1371181ecf090bf6bc0672c9522c0aab
ErickSharp <titaninmc@gmail.com>
2022-02-05 12:37:29 -0600

    feat: add ability to serve requested pdf as image

--- src/utilities/utilities.controller.ts
+++ src/utilities/utilities.controller.ts
@@ -1,7 +1,11 @@
 import { Controller, Get, Query, StreamableFile, Response } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { existsSync, readFileSync, rmSync } from 'fs';
 import { contentType } from 'mime-types';
+import { getDocument } from 'pdfjs-dist/legacy/build/pdf';
 import { FileService } from './file.service';
+
+const pdf = require('pdf-poppler');
 
 @ApiTags('UTILITIES')
 @Controller('utility/v1')
@@ -14,13 +18,33 @@ export class UtiliyController {
         description: 'A Streamed PDF',
         type: StreamableFile,
     })
-    async getPdf(@Query('filename') filename: string, @Response({ passthrough: true }) res): Promise<StreamableFile> {
-        return this.fileService.getFileStream('resources/pdfs/', `${filename}`).then((file) => {
-            res.set({
-                'Content-Type': contentType(filename),
-                'Content-Disposition': `attachment; filename=${filename}`,
-            });
-            return file;
+    async getPdf(@Query('filename') filename: string, @Query('pagenumber') pagenumber: number, @Response({ passthrough: true }) res): Promise<StreamableFile> {
+        res.set({
+            'Content-Type': 'image/png',
+            'Content-Disposition': `attachment; filename=out-${pagenumber}.png}`,
+        });
+
+        const options = {
+            format: 'png',
+            out_dir: `${process.cwd()}\\out`,
+            out_prefix: 'out',
+            page: pagenumber,
+            scale: 2048,
+        };
+
+        console.log(process.cwd());
+
+        return pdf.convert(`${process.cwd()}\\resources\\pdfs\\${filename}`, options).then(() => {
+            const expectedFilePath = `out\\out-${pagenumber}.png`;
+
+            if (existsSync(expectedFilePath)) {
+                return this.fileService.getFileStream(`out/out-${pagenumber}.png`, '').then((file) => {
+                    rmSync(expectedFilePath);
+                    return file;
+                });
+            }
+
+            return undefined;
         });
     }
 
@@ -32,6 +56,18 @@ export class UtiliyController {
     })
     async getPdfFileList() {
         return this.fileService.getFolderFilenames('resources/pdfs/');
+    }
+
+    @Get('pdf/numpages')
+    @ApiResponse({
+        status: 200,
+        description: 'Returns the number of pages in the pdf',
+        type: Number,
+    })
+    async getNumberOfPages(@Query('filename') filename: string): Promise<number> {
+        const retrievedFile = readFileSync(`resources\\pdfs\\${filename}`);
+
+        return getDocument({ data: retrievedFile }).promise.then((document) => document.numPages);
     }
 
     @Get('image')
