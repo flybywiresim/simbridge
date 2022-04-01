b95fa4be74d3eb8f15dd73d022d9be101f8d0fe3
Sven Czarnian <devel@svcz.de>
2022-04-01 23:55:56 +0200

    add an API getter to check if the map exists

--- apps/server/src/terrain/terrain.controller.ts
+++ apps/server/src/terrain/terrain.controller.ts
@@ -6,4 +6,18 @@ import { TerrainService } from './terrain.service';
 @Controller('api/v1/terrain')
 export class TerrainController {
     constructor(private terrainService: TerrainService) {}
+
+    @Get('terrainmapExists')
+    @ApiResponse({
+        status: 200,
+        description: 'the terrainmap data exists',
+        type: Boolean,
+    })
+    @ApiResponse({
+        status: 404,
+        description: 'unable to find the terrainmap data',
+    })
+    async getTerrainmapExists(): Promise<Boolean> {
+        return this.terrainService.terrainmapExists();
+    }
 }
