51f4b0d71c4b5e96275eba3883e199d113bd8b6a
Sven Czarnian <devel@svcz.de>
2022-04-02 00:08:46 +0200

    publish the map data

--- apps/server/src/terrain/terrain.controller.ts
+++ apps/server/src/terrain/terrain.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { TerrainService } from './terrain.service';
+import { TerrainmapInfo } from './dto/terrainmapinfo.dto';
 
 @ApiTags('TERRAIN')
 @Controller('api/v1/terrain')
@@ -19,5 +20,33 @@ export class TerrainController {
     })
     async getTerrainmapExists(): Promise<Boolean> {
         return this.terrainService.terrainmapExists();
+    }
+
+    @Get('mapinfo')
+    @ApiResponse({
+        status: 200,
+        description: 'the terrainmap information',
+        type: TerrainmapInfo,
+    })
+    @ApiResponse({
+        status: 404,
+        description: 'unable to find the terrainmap information',
+    })
+    async getTerrainmapInfo(): Promise<TerrainmapInfo> {
+        return this.terrainService.Terrainmap.then((map) => {
+            const retval = new TerrainmapInfo();
+
+            if (map !== undefined) {
+                retval.mostNorth = map.LatitudeRange[1];
+                retval.mostSouth = map.LatitudeRange[0];
+                retval.mostWest = map.LongitudeRange[0];
+                retval.mostEast = map.LongitudeRange[1];
+                retval.latitudinalStep = map.AngularSteps[0];
+                retval.longitudinalStep = map.AngularSteps[1];
+                retval.elevationResolution = map.ElevationResolution;
+            }
+
+            return retval;
+        });
     }
 }
