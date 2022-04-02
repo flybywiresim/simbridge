35f9e02db259df26331aec7a37c61c0d61d313c3
Sven Czarnian <devel@svcz.de>
2022-04-02 22:25:20 +0200

    initialize the world grid

--- apps/server/src/terrain/manager/worldmap.ts
+++ apps/server/src/terrain/manager/worldmap.ts
@@ -1,4 +1,6 @@
 import { Terrainmap } from '../mapformat/terrainmap';
+import { Tile } from '../mapformat/tile';
+import { ElevationGrid } from '../mapformat/elevationgrid';
 import { Configuration } from '../dto/configuration.dto';
 
 export class Worldmap {
@@ -8,7 +10,32 @@ export class Worldmap {
 
     private visibilityRange: number = 250;
 
+    private static findTileIndex(tiles: Tile[], latitude: number, longitude: number): number {
+        for (let i = 0; i < tiles.length; ++i) {
+            if (tiles[i].Southwest[0] === latitude && tiles[i].Southwest[1] === longitude) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
     constructor(mapfile: Terrainmap) {
+        this.terrainmap = mapfile;
+
+        for (let lat = -90; lat < 90; lat += mapfile.AngularSteps[0]) {
+            this.grid.push([]);
+
+            for (let lon = -180; lon < 180; lon += mapfile.AngularSteps[1]) {
+                this.grid[this.grid.length - 1].push({
+                    southwest: { latitude: lat, longitude: lon },
+                    tileIndex: Worldmap.findTileIndex(mapfile.Tiles, lat, lon),
+                    elevationmap: undefined,
+                });
+            }
+        }
+    }
+
     public configure(config: Configuration) {
         this.visibilityRange = config.visibilityRange;
     }
