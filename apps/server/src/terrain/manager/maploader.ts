e29445d96e0095b532a6dcea48b5fb861e21e7c4
Sven Czarnian <devel@svcz.de>
2022-04-03 16:55:50 +0200

    use a helper class for the projection function

--- apps/server/src/terrain/manager/maploader.ts
+++ apps/server/src/terrain/manager/maploader.ts
@@ -1,19 +1,7 @@
 // import { parentPort, workerData } from 'worker_threads';
 import { Worldmap } from './worldmap';
 import { Position } from '../dto/position.dto';
-
-function project(latitude: number, longitude: number, distance: number, bearing: number): { latitude: number, longitude: number } {
-    const earthRadius = 6371000;
-    const lat1 = latitude * (Math.PI / 180);
-    const lon1 = longitude * (Math.PI / 180);
-    const brg = bearing * (Math.PI / 180);
-    const ratio = distance / earthRadius;
-
-    const lat2 = Math.asin(Math.sin(lat1) * Math.cos(ratio) + Math.cos(lat1) * Math.sin(ratio) * Math.cos(brg));
-    const lon2 = lon1 + Math.atan2(Math.sin(brg) * Math.sin(ratio) * Math.cos(lat1), Math.cos(ratio) - Math.sin(lat1) * Math.sin(lat2));
-
-    return { latitude: (lat2 * 180) / Math.PI, longitude: (lon2 * 180) / Math.PI };
-}
+import { WGS84 } from '../utils/wgs84';
 
 function findTileIndices(world: Worldmap, latitude: number, longitude0: number, longitude1: number): { row: number, column: number }[] {
     const indices: { row: number, column: number }[] = [];
@@ -29,8 +17,8 @@ function findTileIndices(world: Worldmap, latitude: number, longitude0: number, 
 }
 
 export function loadTiles(world: Worldmap, position: Position) {
-    const southwest = project(position.latitude, position.longitude, world.VisibilityRange * 1852, 225);
-    const northeast = project(position.latitude, position.longitude, world.VisibilityRange * 1852, 45);
+    const southwest = WGS84.project(position.latitude, position.longitude, world.VisibilityRange * 1852, 225);
+    const northeast = WGS84.project(position.latitude, position.longitude, world.VisibilityRange * 1852, 45);
 
     // wrap around at 180°
     let tileIndices: { row: number, column: number }[] = [];
---
+++ apps/server/src/terrain/utils/wgs84.ts
@@ -0,0 +1,14 @@
+export class WGS84 {
+    public static project(latitude: number, longitude: number, distance: number, bearing: number): { latitude: number, longitude: number } {
+        const earthRadius = 6371000;
+        const lat1 = latitude * (Math.PI / 180);
+        const lon1 = longitude * (Math.PI / 180);
+        const brg = bearing * (Math.PI / 180);
+        const ratio = distance / earthRadius;
+
+        const lat2 = Math.asin(Math.sin(lat1) * Math.cos(ratio) + Math.cos(lat1) * Math.sin(ratio) * Math.cos(brg));
+        const lon2 = lon1 + Math.atan2(Math.sin(brg) * Math.sin(ratio) * Math.cos(lat1), Math.cos(ratio) - Math.sin(lat1) * Math.sin(lat2));
+
+        return { latitude: (lat2 * 180) / Math.PI, longitude: (lon2 * 180) / Math.PI };
+    }
+}
