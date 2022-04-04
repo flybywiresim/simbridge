3dd7c29a29d1b78ac8bf520b597696c0b15e88a6
Sven Czarnian <devel@svcz.de>
2022-04-04 16:42:44 +0200

    fix a crash

--- apps/server/src/terrain/mapformat/elevationgrid.ts
+++ apps/server/src/terrain/mapformat/elevationgrid.ts
@@ -20,7 +20,7 @@ export class ElevationGrid {
     public worldToGridIndices(coordinate: { latitude: number, longitude: number }): { row: number, column: number } {
         const latRange = this.northeast.latitude - this.southwest.latitude;
         const latDelta = coordinate.latitude - this.southwest.latitude;
-        const row = this.Rows - Math.floor((latDelta / latRange) * this.Rows);
+        const row = this.Rows - Math.floor((latDelta / latRange) * this.Rows) - 1;
 
         const lonRange = this.northeast.longitude - this.southwest.longitude;
         const lonDelta = coordinate.longitude - this.southwest.longitude;
