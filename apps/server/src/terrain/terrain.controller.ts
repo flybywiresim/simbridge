be5dca850e07e11b09016e7ca35f39dddd7bd677
Sven Czarnian <devel@svcz.de>
2022-04-04 16:14:38 +0200

    define the API to create the ND map

--- apps/server/src/terrain/terrain.controller.ts
+++ apps/server/src/terrain/terrain.controller.ts
@@ -4,6 +4,7 @@ import { TerrainService } from './terrain.service';
 import { ConfigurationDto } from './dto/configuration.dto';
 import { PositionDto } from './dto/position.dto';
 import { NDViewDto } from './dto/ndview.dto';
+import { NDMapDto } from './dto/ndmap.dto';
 
 const sharp = require('sharp');
 
@@ -68,7 +69,7 @@ export class TerrainController {
         this.presentHeading = position.heading;
     }
 
-    @Get('ndmap')
+    @Post('ndmap')
     @ApiBody({
         description: 'The new connection containing the flight number and current location',
         type: NDViewDto,
@@ -76,66 +77,33 @@ export class TerrainController {
     @ApiResponse({
         status: 200,
         description: 'The current ND map',
-        type: Buffer,
+        type: NDMapDto,
+    })
+    @ApiResponse({
+        status: 400,
+        description: 'No ND map available',
     })
     @ApiProduces('image/png')
-    async createNDMap(@Body() config: NDViewDto): Promise<Buffer> {
+    async createNDMap(@Body() config: NDViewDto): Promise<NDMapDto> {
         const { buffer, rows, columns } = this.terrainService.MapManager.createMapND(config);
-        let pngBuffer: Buffer | undefined = undefined;
+        if (rows === 0 || columns === 0) {
+            throw new BadRequestException('Unable to create the ND map');
+        }
+
+        const response = new NDMapDto();
+        response.width = columns;
+        response.height = rows;
 
         if (config.rotateAroundHeading === true) {
-            pngBuffer = await sharp(buffer, { raw: { width: columns, height: rows, channels: 3 } })
+            const { data, _ } = await sharp(new Uint8ClampedArray(buffer), { raw: { width: columns, height: rows, channels: 3 } })
                 .rotate(-1 * this.presentHeading)
-                .toFormat('png')
-                .toBuffer();
+                .raw()
+                .toBuffer({ resolveWithObject: true });
+            response.pixels = Buffer.from(data.buffer, 'binary').toString('base64');
         } else {
-            pngBuffer = await sharp(buffer, { raw: { width: columns, height: rows, channels: 3 } })
-                .toFormat('png')
-                .toBuffer();
+            response.pixels = Buffer.from(buffer).toString('base64');
         }
 
-        return pngBuffer;
-    }
-
-    @Get('tile')
-    @ApiResponse({
-        status: 200,
-        description: 'The elevation grid of a tile',
-    })
-    @ApiProduces('image/png')
-    async getTile(@Query('lat') latStr: string, @Query('lon') lonStr: string): Promise<void> {
-        const map = this.terrainService.Terrainmap;
-
-        if (map !== undefined) {
-            const lat = parseInt(latStr);
-            const lon = parseInt(lonStr);
-
-            for (let i = 0; i < map.Tiles.length; ++i) {
-                if (map.Tiles[i].Southwest.latitude === lat && map.Tiles[i].Southwest.longitude === lon) {
-                    const grid = map.Tiles[i].elevationGrid();
-
-                    if (grid.Columns !== 0 && grid.Rows !== 0) {
-                        let maxElev = 0;
-                        grid.Grid.forEach((row) => {
-                            row.forEach((cell) => {
-                                if (maxElev < cell) {
-                                    maxElev = cell;
-                                }
-                            });
-                        });
-                        const pixels = [].concat(...grid.Grid);
-                        for (let i = 0; i < pixels.length; ++i) {
-                            pixels[i] = Math.round((pixels[i] / maxElev) * 255);
-                        }
-
-                        const pixelBuffer = new Uint8ClampedArray(pixels);
-                        const pngBuffer = await sharp(pixelBuffer, { raw: { width: grid.Columns, height: grid.Rows, channels: 1 } })
-                            .toFormat('png')
-                            .toBuffer();
-                        sharp(pngBuffer).toFile('./test.png');
-                    }
-                }
-            }
-        }
+        return response;
     }
 }
