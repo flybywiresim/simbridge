8bac2c50dde8987b895bac3de9eb78528fefd706
Erick Torres <titaninmc@gmail.com>
2022-04-12 10:40:24 -0500

    switch to skia-canvas from node-canvas

--- apps/server/src/utilities/pdfConversion.ts
+++ apps/server/src/utilities/pdfConversion.ts
@@ -1,10 +1,10 @@
-import { createCanvas } from 'canvas';
+import { Canvas } from 'skia-canvas';
 
 import { PDFDocumentProxy } from 'pdfjs-dist/legacy/build/pdf';
 
 class NodeCanvasFactory {
     create(width: number, height: number) {
-        const canvas = createCanvas(width, height);
+        const canvas = new Canvas(width, height);
         const context = canvas.getContext('2d');
         return {
             canvas,
@@ -48,7 +48,7 @@ export const pdfToPng = async (document: PDFDocumentProxy, pageNumber: any, scal
         const renderTask = page.render(renderContext);
         await renderTask.promise;
         // Convert the canvas to an image buffer.
-        const image = canvasAndContext.canvas.toBuffer();
+        const image = await canvasAndContext.canvas.toBuffer('png');
 
         // Release page resources.
         page.cleanup();
