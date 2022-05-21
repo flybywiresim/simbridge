6038229d1ef0aab63e6b6ac4af76b2ef6cd01031
Sven Czarnian <devel@svcz.de>
2022-05-21 23:30:08 +0200

    fix the renderer

--- apps/server/src/terrain/utils/ndrenderer.ts
+++ apps/server/src/terrain/utils/ndrenderer.ts
@@ -50,74 +50,109 @@ export class NDRenderer {
 
         const start = new Date().getTime();
 
-        const radius = Math.round((this.ViewConfig.viewRadius * 1852) / this.ViewConfig.meterPerPixel + 0.5);
-        let size = radius * 2;
-        const buffer = new Uint8ClampedArray(size * size * 3);
-        buffer.fill(0, 0, size * size * 3);
+        // calculate the source dimensions to create the initial map
+        const radiusPixels = Math.round((this.ViewConfig.viewRadius * 1852) / this.ViewConfig.meterPerPixel + 0.5);
+        const mapSize = radiusPixels * 2;
 
-        const viewSouthwest = WGS84.project(position.latitude, position.longitude, this.ViewConfig.viewRadius * 1852 * 2, 225);
-        const viewNortheast = WGS84.project(position.latitude, position.longitude, this.ViewConfig.viewRadius * 1852 * 2, 45);
-        const latitudeStep = (viewNortheast.latitude - viewSouthwest.latitude) / size;
-        const longitudeStep = (viewNortheast.longitude - viewSouthwest.longitude) / size;
+        // create the source buffer
+        const sourceBuffer = new Uint8ClampedArray(mapSize * mapSize * 3);
+        sourceBuffer.fill(0, 0, mapSize * mapSize * 3);
 
-        let even = false;
-        let color: { r: number, g: number, b: number } = { r: 0, g: 0, b: 0 };
-        const coordinate = { latitude: viewNortheast.latitude, longitude: viewSouthwest.longitude };
-        for (let y = 0; y < size; y += 2) {
-            even = !even;
-            for (let x = even ? 1 : 0; x < size; x += 2) {
-                const distance = Math.sqrt((x - radius) ** 2 + (y - radius) ** 2);
-                if (distance - 1 > radius) {
-                    coordinate.longitude += longitudeStep;
+        let viewSouthwest = null;
+        let viewNortheast = null;
+        let latitudeStep = 1;
+        let longitudeStep = 1;
+
+        viewSouthwest = WGS84.project(position.latitude, position.longitude, this.ViewConfig.viewRadius * 1852, 225);
+        viewNortheast = WGS84.project(position.latitude, position.longitude, this.ViewConfig.viewRadius * 1852, 45);
+        latitudeStep = (viewNortheast.latitude - viewSouthwest.latitude) / mapSize;
+        longitudeStep = (viewNortheast.longitude - viewSouthwest.longitude) / mapSize;
+
+        // correct the offset due to inaccurate projection
+        const offsetLat = position.latitude - (viewSouthwest.latitude + (viewNortheast.latitude - viewSouthwest.latitude) / 2);
+        const offsetLon = position.longitude - (viewSouthwest.longitude + (viewNortheast.longitude - viewSouthwest.longitude) / 2);
+
+        viewSouthwest.latitude += offsetLat;
+        viewSouthwest.longitude += offsetLon;
+        viewNortheast.latitude += offsetLat;
+        viewNortheast.longitude += offsetLon;
+
+        let { latitude } = viewNortheast;
+        for (let y = 0; y < mapSize; ++y) {
+            let { longitude } = viewSouthwest;
+
+            for (let x = 0; x < mapSize; ++x) {
+                const distance = Math.sqrt((x - radiusPixels) ** 2 + (y - radiusPixels) ** 2);
+                if (distance > radiusPixels) {
+                    longitude += longitudeStep;
                     continue;
                 }
 
-                const worldIndex = this.worldmap.worldMapIndices(coordinate.latitude, coordinate.longitude);
+                const worldIdx = this.worldmap.worldMapIndices(latitude, longitude);
+                const tile = this.worldmap.Grid[worldIdx.row][worldIdx.column];
 
-                if (this.worldmap.Grid[worldIndex.row][worldIndex.column].tileIndex === -1 || this.worldmap.Grid[worldIndex.row][worldIndex.column].elevationmap === undefined) {
-                    color = NDRenderer.colorize(0, 0);
-                } else {
-                    const { row, column } = this.worldmap.Grid[worldIndex.row][worldIndex.column].elevationmap.worldToGridIndices(coordinate);
-                    const gridBuffer = new Int32Array(this.worldmap.Grid[worldIndex.row][worldIndex.column].elevationmap.Grid);
-                    color = NDRenderer.colorize(gridBuffer[this.worldmap.Grid[worldIndex.row][worldIndex.column].elevationmap.Columns * row + column], position.altitude);
+                let color = { r: 0, g: 0, b: 0 };
+                if (tile.tileIndex === -1) {
+                    color = NDRenderer.colorize(-1, 0);
+                } else if (tile.elevationmap !== undefined) {
+                    const gridBuffer = new Int16Array(tile.elevationmap.Grid);
+                    const mapIdx = tile.elevationmap.worldToGridIndices({ latitude, longitude });
+                    color = NDRenderer.colorize(gridBuffer[mapIdx.row * tile.elevationmap.Columns + mapIdx.column], position.altitude);
                 }
 
-                buffer[(y * size + x) * 3 + 0] = color.r;
-                buffer[(y * size + x) * 3 + 1] = color.g;
-                buffer[(y * size + x) * 3 + 2] = color.b;
+                sourceBuffer[(y * mapSize + x) * 3 + 0] = color.r;
+                sourceBuffer[(y * mapSize + x) * 3 + 1] = color.g;
+                sourceBuffer[(y * mapSize + x) * 3 + 2] = color.b;
 
-                coordinate.longitude += longitudeStep;
+                longitude += longitudeStep;
             }
 
-            coordinate.longitude = viewSouthwest.longitude;
-            coordinate.latitude += latitudeStep;
+            latitude -= latitudeStep;
         }
 
-        const rotatedRaw = await sharp(new Uint8ClampedArray(buffer), { raw: { width: size, height: size, channels: 3 } })
-            .rotate(this.ViewConfig.rotateAroundHeading === true ? -1 * position.heading : 0)
-            .raw()
-            .toBuffer({ resolveWithObject: true });
+        if (this.ViewConfig.rotateAroundHeading) {
+            const { data, info } = await sharp(new Uint8ClampedArray(sourceBuffer), { raw: { width: mapSize, height: mapSize, channels: 3 } })
+                .rotate(-1 * position.heading)
+                .raw()
+                .toBuffer({ resolveWithObject: true });
 
-        const targetHeight = this.ViewConfig.semicircleRequired === true ? Math.round(size / 2) : size;
-        const sizeRatio = this.ViewConfig.screenHeight !== targetHeight ? targetHeight / this.ViewConfig.screenHeight : 1;
-        if (size !== this.ViewConfig.screenWidth) {
-            size = Math.round(this.ViewConfig.screenWidth * sizeRatio + 0.5);
+            const topOffset = Math.round((info.height - mapSize) / 2 + 0.5);
+            let leftOffset = Math.round((info.width - mapSize) / 2 + 0.5);
+
+            let result = null;
+            if (this.ViewConfig.semicircleRequired) {
+                if (this.ViewConfig.maxWidth < mapSize) {
+                    leftOffset += (mapSize - this.ViewConfig.maxWidth) / 2;
+                }
+
+                result = await sharp(new Uint8ClampedArray(data.buffer), { raw: { width: info.width, height: info.height, channels: 3 } })
+                    .extract({ width: Math.min(this.ViewConfig.maxWidth, mapSize), height: radiusPixels, left: leftOffset, top: topOffset })
+                    .raw()
+                    .toBuffer({ resolveWithObject: true });
+            } else {
+                result = await sharp(new Uint8ClampedArray(data.buffer), { raw: { width: info.width, height: info.height, channels: 3 } })
+                    .extract({ width: mapSize, height: mapSize, left: leftOffset, top: topOffset })
+                    .raw()
+                    .toBuffer({ resolveWithObject: true });
+            }
+
+            const retval = new SharedArrayBuffer(result.info.width * result.info.height * 3);
+            const dest = new Uint8ClampedArray(retval);
+            dest.set(new Uint8ClampedArray(result.data.buffer), 0);
+
+            const delta = new Date().getTime() - start;
+            console.log(`Created ND map in ${delta / 1000} seconds`);
+
+            return { buffer: retval, rows: result.info.height, columns: result.info.width };
         }
-        const offset = Math.round((rotatedRaw.info.width - size) / 2);
 
-        const { data, info } = await sharp(new Uint8ClampedArray(rotatedRaw.data.buffer), { raw: { width: rotatedRaw.info.width, height: rotatedRaw.info.height, channels: 3 } })
-            .extract({ width: Math.min(size, rotatedRaw.info.width), height: Math.min(targetHeight, rotatedRaw.info.height), left: offset, top: 0 })
-            .resize({ fit: sharp.fit.contain, height: this.ViewConfig.screenHeight })
-            .raw()
-            .toBuffer({ resolveWithObject: true });
-
-        const retval = new SharedArrayBuffer(info.width * info.height * 3);
+        const retval = new SharedArrayBuffer(mapSize * mapSize * 3);
         const dest = new Uint8ClampedArray(retval);
-        dest.set(new Uint8ClampedArray(data.buffer), 0);
+        dest.set(sourceBuffer, 0);
 
         const delta = new Date().getTime() - start;
         console.log(`Created ND map in ${delta / 1000} seconds`);
 
-        return { buffer: retval, rows: info.height, columns: info.width };
+        return { buffer: retval, rows: mapSize, columns: mapSize };
     }
 }
