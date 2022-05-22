f220813fc18c7507cf3f9a80f028f51c346cdf36
Sven Czarnian <devel@svcz.de>
2022-05-22 09:56:47 +0200

    define a class that contains all local map information

---
+++ apps/server/src/terrain/utils/localmap.ts
@@ -0,0 +1,15 @@
+export class LocalMap {
+    public ElevationMap: Int16Array = null;
+
+    public ReferenceElevation: number = 0;
+
+    public MaximumElevation: number = 0;
+
+    public ElevationPercentile85th: number = 0;
+
+    public ElevationPercentile95th: number = 0;
+
+    public ElevationUpperHalf: number = 0;
+
+    public ElevationUpper35Percent: number = 0;
+}
