dcb222cad1d5584d2f154f220b1d6bc2e6bbb04e
Alex McBean <al.mcbean@outlook.com>
2022-07-26 12:15:10 +0100

    Appliocations executable properly configured

---
+++ exeBuild.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    file: 'dist/main.js',
+    icon: 'apps/server/src/assets/images/tail.ico',
+    name: 'fbw-simbridge',
+    description: 'Application for providing external service to FlyByWire aircraft',
+    company: 'FlyByWire Simulations',
+    version: '0.3.9',
+    copyright: 'GNU v3',
+    pkg: {
+        targets: [
+            'node16-win-x64',
+        ],
+        assets: [
+            'node_modules/linebreak/src/classes.trie',
+            'node_modules/skia-canvas/**/*.*',
+            'node_modules/canvas/**/*.*',
+            'node_modules/pdfkit/js/data/Helvetica.afm',
+            'dist/mcdu/**/*',
+            'dist/assets/**/*',
+        ],
+        outputPath: 'build',
+    },
+};
