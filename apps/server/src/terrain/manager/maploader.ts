b6f636eac56c8c77c5e13afd9ce43607e7fdef26
Sven Czarnian <devel@svcz.de>
2022-08-04 22:23:53 +0200

    increase tile management performance by 30-40%

--- apps/server/src/terrain/manager/maploader.ts
+++ apps/server/src/terrain/manager/maploader.ts
@@ -1,57 +1,28 @@
 import { parentPort } from 'worker_threads';
 import { ElevationGrid } from '../mapformat/elevationgrid';
-import { Worldmap, WorldMapData } from './worldmap';
+import { TerrainMap } from '../mapformat/terrainmap';
 import { Tile } from '../mapformat/tile';
-import { PositionDto } from '../dto/position.dto';
-import { WGS84 } from '../utils/wgs84';
 
-function findTileIndices(data: WorldMapData, latitude: number, longitude0: number, longitude1: number): { row: number, column: number }[] {
-    const indices: { row: number, column: number }[] = [];
+let mapdata: TerrainMap = null;
 
-    for (let lon = longitude0; lon < longitude1; lon += data.terrainData.AngularSteps.longitude) {
-        const index = Worldmap.worldMapIndices(data, latitude, lon);
-        if (index !== undefined && Worldmap.validTile(data, index) === true) {
-            indices.push(index);
-        }
-    }
-
-    return indices;
-}
-
-function loadTiles(data: WorldMapData, position: PositionDto, visibilityRange: number) {
-    const southwest = WGS84.project(position.latitude, position.longitude, visibilityRange * 1852, 225);
-    const northeast = WGS84.project(position.latitude, position.longitude, visibilityRange * 1852, 45);
-
-    // wrap around at 180°
-    let tileIndices: { row: number, column: number }[] = [];
-    if (southwest.longitude > northeast.longitude) {
-        for (let lat = southwest.latitude; lat < northeast.latitude; lat += data.terrainData.AngularSteps.latitude) {
-            tileIndices = tileIndices.concat(findTileIndices(data, lat, southwest.longitude, 180));
-            tileIndices = tileIndices.concat(findTileIndices(data, lat, -180, northeast.longitude));
-        }
-    } else {
-        for (let lat = southwest.latitude; lat < northeast.latitude; lat += data.terrainData.AngularSteps.latitude) {
-            tileIndices = tileIndices.concat(findTileIndices(data, lat, southwest.longitude, northeast.longitude));
-        }
-    }
+function loadTiles(tileIndices: { row: number, column: number, tileIndex: number }[]) {
+    const retval: { row: Number, column: Number, grid: ElevationGrid }[] = [];
 
     // load all missing tiles
-    const retval: { row: Number, column: Number, grid: ElevationGrid }[] = [];
     tileIndices.forEach((index) => {
-        if (Worldmap.validTile(data, index) === true && data.grid[index.row][index.column].elevationmap === undefined) {
-            const map = Tile.loadElevationGrid(data.terrainData.Tiles[data.grid[index.row][index.column].tileIndex]);
-            map.ElevationMap = null;
-            retval.push({ row: index.row, column: index.column, grid: map });
-        } else {
-            retval.push({ row: index.row, column: index.column, grid: null });
-        }
+        const map = Tile.loadElevationGrid(mapdata.Tiles[index.tileIndex]);
+        retval.push({ row: index.row, column: index.column, grid: map });
     });
 
     return retval;
 }
 
-parentPort.on('message', (data: { data: WorldMapData, position: PositionDto, visibilityRange: number }) => {
-    parentPort.postMessage(
-        loadTiles(data.data, data.position, data.visibilityRange),
-    );
+parentPort.on('message', (data: { type: string, instance: any }) => {
+    if (data.type === 'MAP') {
+        mapdata = data.instance as TerrainMap;
+    } else if (data.type === 'TILES' && mapdata !== null) {
+        parentPort.postMessage(
+            loadTiles(data.instance as { row: number, column: number, tileIndex: number }[]),
+        );
+    }
 });
--- apps/server/src/terrain/manager/worldmap.ts
+++ apps/server/src/terrain/manager/worldmap.ts
@@ -1,5 +1,6 @@
 import { Worker } from 'worker_threads';
 import * as path from 'path';
+import { WGS84 } from '../utils/wgs84';
 import { ElevationGrid } from '../mapformat/elevationgrid';
 import { TerrainMap } from '../mapformat/terrainmap';
 import { Tile } from '../mapformat/tile';
@@ -15,11 +16,19 @@ export interface WorldMapData {
     grid: { southwest: { latitude: number, longitude: number }, tileIndex: number, elevationmap: undefined | ElevationGrid }[][];
 }
 
+interface TileLoadingData {
+    whitelist: { row: number, column: number }[];
+
+    loadlist: { row: number, column: number, tileIndex: number }[];
+}
+
 export class Worldmap {
     public data: WorldMapData = {
         terrainData: undefined,
         grid: [],
     };
+
+    private tileLoaderWhitelist: { row: number, column: number }[] = [];
 
     private tileLoaderWorker: Worker;
 
@@ -65,6 +74,7 @@ export class Worldmap {
         }
 
         this.tileLoaderWorker = new Worker(path.resolve(__dirname, './maploader.js'));
+        this.tileLoaderWorker.postMessage({ type: 'MAP', instance: this.data.terrainData });
         this.tileLoaderWorker.on('message', (result) => {
             const loadedTiles: { row: number, column: number }[] = [];
 
@@ -75,7 +85,7 @@ export class Worldmap {
                 }
             });
 
-            this.cleanupElevationCache(loadedTiles);
+            this.cleanupElevationCache();
             this.tileLoadingInProgress = false;
         });
 
@@ -136,6 +146,34 @@ export class Worldmap {
         }
     }
 
+    private findTileIndices(latitude: number, longitude0: number, longitude1: number): { row: number, column: number }[] {
+        const indices: { row: number, column: number }[] = [];
+
+        for (let lon = longitude0; lon < longitude1; lon += this.data.terrainData.AngularSteps.longitude) {
+            const index = Worldmap.worldMapIndices(this.data, latitude, lon);
+            if (index !== undefined && Worldmap.validTile(this.data, index) === true) {
+                indices.push(index);
+            }
+        }
+
+        return indices;
+    }
+
+    private filterTileIndexCandidates(tileIndices: { row: number, column: number}[]): TileLoadingData {
+        let loadlist: { row: number, column: number, tileIndex: number }[] = [];
+
+        tileIndices.forEach((index) => {
+            if (this.data.grid[index.row][index.column].elevationmap === undefined) {
+                loadlist = loadlist.concat({ row: index.row, column: index.column, tileIndex: this.data.grid[index.row][index.column].tileIndex });
+            }
+        });
+
+        return {
+            whitelist: tileIndices,
+            loadlist,
+        };
+    }
+
     public async updatePosition(position: PositionDto): Promise<void> {
         if (this.tileLoadingInProgress) {
             return;
@@ -144,7 +182,34 @@ export class Worldmap {
         this.tileLoadingInProgress = true;
         this.presentPosition = position;
 
-        this.tileLoaderWorker.postMessage({ data: this.data, position: this.presentPosition, visibilityRange: this.visibilityRange });
+        const southwest = WGS84.project(position.latitude, position.longitude, this.visibilityRange * 1852, 225);
+        const northeast = WGS84.project(position.latitude, position.longitude, this.visibilityRange * 1852, 45);
+        const tiles: TileLoadingData = { whitelist: [], loadlist: [] };
+
+        // wrap around at 180°
+        if (southwest.longitude > northeast.longitude) {
+            for (let lat = southwest.latitude; lat < northeast.latitude; lat += this.data.terrainData.AngularSteps.latitude) {
+                let indices = this.filterTileIndexCandidates(this.findTileIndices(lat, southwest.longitude, 180));
+                tiles.loadlist = tiles.loadlist.concat(indices.loadlist);
+                tiles.whitelist = tiles.whitelist.concat(indices.whitelist);
+                indices = this.filterTileIndexCandidates(this.findTileIndices(lat, -180, northeast.longitude));
+                tiles.loadlist = tiles.loadlist.concat(indices.loadlist);
+                tiles.whitelist = tiles.whitelist.concat(indices.whitelist);
+            }
+        } else {
+            for (let lat = southwest.latitude; lat < northeast.latitude; lat += this.data.terrainData.AngularSteps.latitude) {
+                const indices = this.filterTileIndexCandidates(this.findTileIndices(lat, southwest.longitude, northeast.longitude));
+                tiles.loadlist = tiles.loadlist.concat(indices.loadlist);
+                tiles.whitelist = tiles.whitelist.concat(indices.whitelist);
+            }
+        }
+
+        if (tiles.loadlist.length !== 0) {
+            this.tileLoaderWhitelist = tiles.whitelist;
+            this.tileLoaderWorker.postMessage({ type: 'TILES', instance: tiles.loadlist });
+        } else {
+            this.tileLoadingInProgress = false;
+        }
     }
 
     public static worldMapIndices(data: WorldMapData, latitude: number, longitude: number): { row: number, column: number } | undefined {
@@ -167,20 +232,19 @@ export class Worldmap {
     }
 
     public setElevationMap(index: { row: number, column: number }, map: ElevationGrid): void {
-        if (Worldmap.validTile(this.data, index) === true && this.data.grid[index.row][index.column].elevationmap === undefined) {
+        if (Worldmap.validTile(this.data, index) === true) {
             this.data.grid[index.row][index.column].elevationmap = map;
-            this.data.grid[index.row][index.column].elevationmap.ElevationMap = new Int16Array(this.data.grid[index.row][index.column].elevationmap.Grid);
         }
     }
 
-    public cleanupElevationCache(whitelist: { row: number, column: number }[]): void {
+    public cleanupElevationCache(): void {
         for (let row = 0; row < this.data.grid.length; ++row) {
             for (let col = 0; col < this.data.grid[row].length; ++col) {
-                const idx = whitelist.findIndex((element) => element.column === col && element.row === row);
+                const idx = this.tileLoaderWhitelist.findIndex((element) => element.column === col && element.row === row);
                 if (idx === -1) {
                     this.data.grid[row][col].elevationmap = undefined;
                 } else {
-                    whitelist.splice(idx, 1);
+                    this.tileLoaderWhitelist.splice(idx, 1);
                 }
             }
         }
