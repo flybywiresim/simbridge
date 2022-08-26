8bd3772672a78843c3c78cb6d26f60cb311392aa
Vivek Bharatha <vivek.bharatha@gmail.com>
2022-08-26 20:27:44 +0400

    Systray graceful kill (#39)
    * feat: adds shutdown listener
    * feat: moves systray to service and graceful kill

--- apps/server/src/utilities/utilities.module.ts
+++ apps/server/src/utilities/utilities.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UtiliyController } from './utilities.controller';
 import { FileService } from './file.service';
 import { PrinterService } from './printer.service';
+import { ShutDownService } from './shutdown.service';
+import { SysTrayService } from './systray.service';
 
 @Module({
     controllers: [UtiliyController],
-    providers: [FileService, PrinterService],
+    providers: [FileService, PrinterService, ShutDownService, SysTrayService],
     exports: [FileService, PrinterService],
 })
 export class UtilitiesModule {}
