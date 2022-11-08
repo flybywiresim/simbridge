38012ef9cc765886cf59af8aeba543b26df80b6c
Frank Kopp <frank@familie-kopp.de>
2022-11-08 17:57:59 +0100

    Added  (configurable) feature to close SimBridge with MSFS

--- apps/server/src/config/server.config.ts
+++ apps/server/src/config/server.config.ts
@@ -8,5 +8,5 @@ export default registerAs('server', () => {
     const configPath = join(process.cwd(), CONFIG_FILENAME);
     const properties = JSON.parse(readFileSync(configPath, 'utf8'));
 
-    return { port: properties.server.port, hidden: properties.server.hidden };
+    return { port: properties.server.port, hidden: properties.server.hidden, closeWithMSFS: properties.server.closeWithMSFS };
 });
