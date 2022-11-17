b600379d1b0d70fcfa57a8ee744e34323955268a
Sven Czarnian <devel@svcz.de>
2022-11-17 16:41:28 +0100

    add a distance function

--- apps/server/src/terrain/utils/gpu/helper.ts
+++ apps/server/src/terrain/utils/gpu/helper.ts
@@ -12,6 +12,20 @@ export function rad2deg(radian: number): number {
     return radian * (180.0 / Math.PI);
 }
 
+export function distanceWgs84(latitude0: number, longitude0: number, latitude1: number, longitude1: number): number {
+    const deltaLatitude = deg2rad(latitude1 - latitude0);
+    const deltaLongitude = deg2rad(longitude1 - longitude0);
+    const latitude0radian = deg2rad(latitude0);
+    const latitude1radian = deg2rad(latitude1);
+
+    const a = 0.5 - Math.cos(deltaLatitude) * 0.5
+        + Math.cos(latitude0radian) * Math.cos(latitude1radian)
+        * (1.0 - Math.cos(deltaLongitude)) * 0.5;
+
+    const distanceMetres = 12742020.0 * Math.asin(Math.sqrt(a));
+    return distanceMetres * 0.000539957;
+}
+
 export function projectWgs84(latitude: number, longitude: number, bearing: number, distance: number): [number, number] {
     const latRad = deg2rad(latitude);
     const longRad = deg2rad(longitude);
@@ -114,7 +128,15 @@ export const registerHelperFunctions = (gpu: GPU): void => {
         argumentTypes: { degree: 'Float' },
         returnType: 'Float',
     });
-
+    gpu.addFunction(distanceWgs84, {
+        argumentTypes: {
+            latitude0: 'Float',
+            longitude0: 'Float',
+            latitude1: 'Float',
+            longitude1: 'Float',
+        },
+        returnType: 'Float',
+    });
     gpu.addFunction(projectWgs84, {
         argumentTypes: {
             latitude: 'Float',
