be7b9abc25309d91d7c67f3c1fab6979942295db
Alex McBean <al.mcbean@outlook.com>
2022-11-26 16:04:57 +0000

    version bump

--- exeBuild.config.js
+++ exeBuild.config.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'fbw-simbridge',
     description: 'FlyByWire Simbridge',
     company: 'FlyByWire Simulations',
-    version: '0.3.18',
+    version: '0.3.19',
     copyright: 'GNU v3',
     pkg: {
         targets: [
