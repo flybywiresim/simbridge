47950550c11163ab7c4ef7d642460ad9509a2b41
Sven Czarnian <devel@svcz.de>
2022-12-06 12:14:51 +0100

    add functions to send the data

--- apps/server/src/terrain/processing/simconnect.ts
+++ apps/server/src/terrain/processing/simconnect.ts
@@ -1,9 +1,82 @@
-import { open, Protocol, SimConnectConnection } from 'node-simconnect';
+import { open, Protocol, SimConnectConnection, SimConnectConstants } from 'node-simconnect';
+import { NavigationDisplayData } from './navigationdisplaydata';
+
+export interface SimConnectParameters {
+    maxNavigationDisplayWidth: number,
+    maxNavigationDisplayHeight: number,
+    colorChannelCount: number,
+}
+
+const enum ClientDataId {
+    NavigationDisplayThresholds = 1000,
+    NavigationDisplayFrame = 1001,
+}
+
+const NavigationDisplayThresholdByteCount = 10;
 
 export class SimConnect {
+    private parameters: SimConnectParameters = null;
+
     private shutdown: boolean = false;
 
     private connection: SimConnectConnection = null;
+
+    private registerNavigationDisplayThresholdData(): void {
+        // see data definition below for byte count
+        this.connection.createClientData(ClientDataId.NavigationDisplayFrame, NavigationDisplayThresholdByteCount, false);
+
+        // image width
+        this.connection.addToClientDataDefinition(
+            ClientDataId.NavigationDisplayThresholds,
+            SimConnectConstants.CLIENTDATAOFFSET_AUTO,
+            SimConnectConstants.CLIENT_DATA_TYPE_INT16,
+        );
+        // image height
+        this.connection.addToClientDataDefinition(
+            ClientDataId.NavigationDisplayThresholds,
+            SimConnectConstants.CLIENTDATAOFFSET_AUTO,
+            SimConnectConstants.CLIENT_DATA_TYPE_INT16,
+        );
+
+        // lowest elevation
+        this.connection.addToClientDataDefinition(
+            ClientDataId.NavigationDisplayThresholds,
+            SimConnectConstants.CLIENTDATAOFFSET_AUTO,
+            SimConnectConstants.CLIENT_DATA_TYPE_INT16,
+        );
+        // lowest elevation mode
+        this.connection.addToClientDataDefinition(
+            ClientDataId.NavigationDisplayThresholds,
+            SimConnectConstants.CLIENTDATAOFFSET_AUTO,
+            SimConnectConstants.CLIENT_DATA_TYPE_INT8,
+        );
+
+        // highest elevation
+        this.connection.addToClientDataDefinition(
+            ClientDataId.NavigationDisplayThresholds,
+            SimConnectConstants.CLIENTDATAOFFSET_AUTO,
+            SimConnectConstants.CLIENT_DATA_TYPE_INT16,
+        );
+        // highest elevation mode
+        this.connection.addToClientDataDefinition(
+            ClientDataId.NavigationDisplayThresholds,
+            SimConnectConstants.CLIENTDATAOFFSET_AUTO,
+            SimConnectConstants.CLIENT_DATA_TYPE_INT8,
+        );
+    }
+
+    private registerNavigationDisplayData(): void {
+        const byteCount = this.parameters.maxNavigationDisplayHeight * this.parameters.maxNavigationDisplayWidth * this.parameters.colorChannelCount;
+        this.connection.createClientData(ClientDataId.NavigationDisplayFrame, byteCount, false);
+
+        for (let i = 0; i < byteCount; i++) {
+            this.connection.addToClientDataDefinition(
+                ClientDataId.NavigationDisplayFrame,
+                SimConnectConstants.CLIENTDATAOFFSET_AUTO,
+                SimConnectConstants.CLIENT_DATA_TYPE_INT8,
+            );
+        }
+    }
 
     private connectToSim() {
         if (this.shutdown) return;
@@ -12,6 +85,9 @@ export class SimConnect {
             .then(({ recvOpen, handle }) => {
                 console.log(`Connected to ${recvOpen.applicationName}`);
                 this.connection = handle;
+
+                this.registerNavigationDisplayThresholdData();
+                this.registerNavigationDisplayData();
 
                 this.connection.on('quit', () => {
                     console.log('Simulator quit!');
@@ -31,12 +107,13 @@ export class SimConnect {
                 });
             })
             .catch((error) => {
-                console.log(`Connection failed: ${error} - Retry in 5 seconds`);
-                setTimeout(() => this.connectToSim(), 5000);
+                console.log(`Connection failed: ${error} - Retry in 10 seconds`);
+                setTimeout(() => this.connectToSim(), 10000);
             });
     }
 
-    constructor() {
+    constructor(parameters: SimConnectParameters) {
+        this.parameters = parameters;
         this.connectToSim();
     }
 
@@ -44,5 +121,46 @@ export class SimConnect {
         this.shutdown = true;
         if (this.connection !== null) this.connection.close();
         this.connection = null;
+    }
+
+    public sendNavigationDisplayThresholds(thresholdData: NavigationDisplayData): void {
+        if (this.connection === null) return;
+
+        const buffer = Buffer.alloc(NavigationDisplayThresholdByteCount);
+
+        // fill the buffer
+        buffer.writeInt16LE(thresholdData.Columns);
+        buffer.writeInt16LE(thresholdData.Rows);
+        buffer.writeInt16LE(thresholdData.MinimumElevation);
+        buffer.writeInt8(thresholdData.MinimumElevationMode);
+        buffer.writeInt16LE(thresholdData.MaximumElevation);
+        buffer.writeInt8(thresholdData.MaximumElevationMode);
+
+        this.connection.setClientData(
+            ClientDataId.NavigationDisplayThresholds,
+            ClientDataId.NavigationDisplayThresholds,
+            0,
+            0,
+            NavigationDisplayThresholdByteCount,
+            buffer,
+        );
+    }
+
+    public sendNavigationDisplayTerrainMapFrame(frame: Uint8ClampedArray): void {
+        if (this.connection === null) return;
+
+        const byteCount = this.parameters.maxNavigationDisplayHeight * this.parameters.maxNavigationDisplayWidth * this.parameters.colorChannelCount;
+        const buffer = Buffer.alloc(byteCount, 0);
+
+        frame.forEach((entry) => buffer.writeInt8(entry));
+
+        this.connection.setClientData(
+            ClientDataId.NavigationDisplayFrame,
+            ClientDataId.NavigationDisplayFrame,
+            0,
+            0,
+            buffer.byteLength,
+            buffer,
+        );
     }
 }
