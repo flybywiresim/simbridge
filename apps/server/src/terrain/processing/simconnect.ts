68878b43d16b3e473ac799468ec8818f2fbc903e
Sven Czarnian <devel@svcz.de>
2022-12-09 10:35:08 +0100

    adapt to the new simconnect library

--- apps/server/src/terrain/processing/simconnect.ts
+++ apps/server/src/terrain/processing/simconnect.ts
@@ -1,188 +1,128 @@
-import { existsSync, readFileSync } from 'fs';
-import { open, Protocol, SimConnectConnection, SimConnectConstants } from 'node-simconnect';
-import { userInfo } from 'os';
+import {
+    ClientData,
+    ClientDataDefinition,
+    ClientDataOffsetAuto,
+    ClientDataType,
+    Connection,
+} from '@devfs/msfs-nodejs';
 import { parentPort } from 'worker_threads';
 import { NavigationDisplayData } from './navigationdisplaydata';
 
-const parser = require('xml2json');
-
-const SimConnectClientName = 'Map handling SimConnect client';
+const SimConnectClientName = 'FBW_SIMBRIDGE_SIMCONNECT';
 
 const enum ClientDataId {
-    NavigationDisplayThresholds,
-    NavigationDisplayFrame,
+    NavigationDisplayThresholds = 1000,
+    NavigationDisplayFrame = 1001,
 }
 
 const NavigationDisplayThresholdByteCount = 10;
+const NavigationDisplayImageBufferSize = 40000;
 
 export class SimConnect {
-    private simConnectPort: number = 500;
-
     private simConnectMaxReceiveSize: number = 16000;
 
     private shutdown: boolean = false;
 
-    private connection: SimConnectConnection = null;
+    private connection: Connection = null;
+
+    private thresholdData: ClientData = null;
+
+    private imageData: ClientData = null;
 
     private registerNavigationDisplayThresholdData(): void {
-        this.connection.mapClientDataNameToID('FBW_SIMBRIDGE_TERRONND_THRESHOLDS', ClientDataId.NavigationDisplayFrame);
+        ClientData.create(
+            'FBW_SIMBRIDGE_TERRONND_THRESHOLDS',
+            ClientDataId.NavigationDisplayThresholds,
+            NavigationDisplayThresholdByteCount,
+            false,
+        ).then((data) => {
+            // image dimensions (width, height)
+            data.addDataDefinition(new ClientDataDefinition(
+                ClientDataId.NavigationDisplayThresholds,
+                ClientDataOffsetAuto,
+                ClientDataType.Int16,
+            ));
+            data.addDataDefinition(new ClientDataDefinition(
+                ClientDataId.NavigationDisplayThresholds,
+                ClientDataOffsetAuto,
+                ClientDataType.Int16,
+            ));
 
-        // see data definition below for byte count
-        this.connection.createClientData(ClientDataId.NavigationDisplayFrame, NavigationDisplayThresholdByteCount, false);
+            // lowest elevation and mode
+            data.addDataDefinition(new ClientDataDefinition(
+                ClientDataId.NavigationDisplayThresholds,
+                ClientDataOffsetAuto,
+                ClientDataType.Int16,
+            ));
+            data.addDataDefinition(new ClientDataDefinition(
+                ClientDataId.NavigationDisplayThresholds,
+                ClientDataOffsetAuto,
+                ClientDataType.Int8,
+            ));
 
-        // image width
-        this.connection.addToClientDataDefinition(
-            ClientDataId.NavigationDisplayThresholds,
-            SimConnectConstants.CLIENTDATAOFFSET_AUTO,
-            SimConnectConstants.CLIENT_DATA_TYPE_INT16,
-        );
-        // image height
-        this.connection.addToClientDataDefinition(
-            ClientDataId.NavigationDisplayThresholds,
-            SimConnectConstants.CLIENTDATAOFFSET_AUTO,
-            SimConnectConstants.CLIENT_DATA_TYPE_INT16,
-        );
+            // highest elevation and mode
+            data.addDataDefinition(new ClientDataDefinition(
+                ClientDataId.NavigationDisplayThresholds,
+                ClientDataOffsetAuto,
+                ClientDataType.Int16,
+            ));
+            data.addDataDefinition(new ClientDataDefinition(
+                ClientDataId.NavigationDisplayThresholds,
+                ClientDataOffsetAuto,
+                ClientDataType.Int8,
+            ));
 
-        // lowest elevation
-        this.connection.addToClientDataDefinition(
-            ClientDataId.NavigationDisplayThresholds,
-            SimConnectConstants.CLIENTDATAOFFSET_AUTO,
-            SimConnectConstants.CLIENT_DATA_TYPE_INT16,
-        );
-        // lowest elevation mode
-        this.connection.addToClientDataDefinition(
-            ClientDataId.NavigationDisplayThresholds,
-            SimConnectConstants.CLIENTDATAOFFSET_AUTO,
-            SimConnectConstants.CLIENT_DATA_TYPE_INT8,
-        );
-
-        // highest elevation
-        this.connection.addToClientDataDefinition(
-            ClientDataId.NavigationDisplayThresholds,
-            SimConnectConstants.CLIENTDATAOFFSET_AUTO,
-            SimConnectConstants.CLIENT_DATA_TYPE_INT16,
-        );
-        // highest elevation mode
-        this.connection.addToClientDataDefinition(
-            ClientDataId.NavigationDisplayThresholds,
-            SimConnectConstants.CLIENTDATAOFFSET_AUTO,
-            SimConnectConstants.CLIENT_DATA_TYPE_INT8,
-        );
+            this.thresholdData = data;
+        }).catch((error) => {
+            parentPort.postMessage({ request: 'LOGERROR', response: `Unable to create the threshold client data: ${error}` });
+            throw error;
+        });
     }
 
     private registerNavigationDisplayData(): void {
-        this.connection.mapClientDataNameToID('FBW_SIMBRIDGE_TERRONND_IMAGE', ClientDataId.NavigationDisplayFrame);
-
-        this.connection.createClientData(ClientDataId.NavigationDisplayFrame, this.simConnectMaxReceiveSize, false);
-
-        this.connection.addToClientDataDefinition(
+        ClientData.create(
+            'FBW_SIMBRIDGE_TERRONND_IMAGE',
             ClientDataId.NavigationDisplayFrame,
-            SimConnectConstants.CLIENTDATAOFFSET_AUTO,
-            this.simConnectMaxReceiveSize,
-        );
+            NavigationDisplayImageBufferSize,
+            false,
+        ).then((data) => {
+            // image dimensions (width, height)
+            data.addDataDefinition(new ClientDataDefinition(
+                ClientDataId.NavigationDisplayThresholds,
+                ClientDataOffsetAuto,
+                NavigationDisplayImageBufferSize,
+            ));
+
+            this.imageData = data;
+        }).catch((error) => {
+            parentPort.postMessage({ request: 'LOGERROR', response: `Unable to create the image client data: ${error}` });
+            throw error;
+        });
     }
 
     private connectToSim() {
         if (this.shutdown) return;
 
-        open(SimConnectClientName, Protocol.FSX_SP2, { remote: { host: 'localhost', port: this.simConnectPort } })
-            .then(({ recvOpen, handle }) => {
-                parentPort.postMessage({ request: 'LOGMESSAGE', response: `Connected to ${recvOpen.applicationName}` });
-                this.connection = handle;
+        this.connection = new Connection();
+        this.connection.open(SimConnectClientName).then(() => {
+            this.registerNavigationDisplayThresholdData();
+            this.registerNavigationDisplayData();
+            console.log('created data');
+        }).catch((error) => {
+            this.connection.close();
+            this.connection = null;
 
-                this.registerNavigationDisplayThresholdData();
-                this.registerNavigationDisplayData();
-
-                this.connection.on('quit', () => {
-                    parentPort.postMessage({ request: 'LOGMESSAGE', response: 'Simulator quit!' });
-
-                    if (this.connection !== null) this.connection.close();
-                    this.connection = null;
-
-                    this.connectToSim();
-                });
-                this.connection.on('close', () => {
-                    parentPort.postMessage({ request: 'LOGERROR', response: 'Connection closed unexpectedly!' });
-
-                    if (this.connection !== null) this.connection.close();
-                    this.connection = null;
-
-                    this.connectToSim();
-                });
-            })
-            .catch((error) => {
-                parentPort.postMessage({ request: 'LOGERROR', response: `Connection failed: ${error} - Retry in 10 seconds` });
-                setTimeout(() => this.connectToSim(), 10000);
-            });
-    }
-
-    private validateSimConnectConnectionEntry(entry: any): boolean {
-        if (entry.Protocol === 'IPv4' && entry.Port !== undefined && entry.Port > 0 && entry.MaxRecvSize > 0) {
-            parentPort.postMessage({
-                request: 'LOGMESSAGE',
-                response: `Found valid Comm-configuration. Using port ${entry.Port} with a maximum receive size of ${entry.MaxRecvSize}`,
-            });
-
-            this.simConnectPort = entry.Port;
-            this.simConnectMaxReceiveSize = parseInt(entry.MaxRecvSize);
-
-            return true;
-        }
-
-        return false;
-    }
-
-    private loadSimConnectConfiguration(): void {
-        const { homedir } = userInfo();
-
-        const msStoreLocation = `${homedir}\\AppData\\Local\\Packages\\Microsoft.FlightSimulator_8wekyb3d8bbwe\\LocalCache\\SimConnect.xml`;
-        const steamLocation = `${homedir}\\AppData\\Roaming\\Microsoft Flight Simulator\\SimConnect.xml`;
-        let filename = null;
-
-        if (existsSync(msStoreLocation)) {
-            parentPort.postMessage({ request: 'LOGMESSAGE', response: 'MS Store version detected' });
-            filename = msStoreLocation;
-        } else if (existsSync(steamLocation)) {
-            parentPort.postMessage({ request: 'LOGMESSAGE', response: 'Steam version detected' });
-            filename = steamLocation;
-        } else {
-            parentPort.postMessage({ request: 'LOGWARN', response: 'No SimConnect.xml file found. Trying port 500' });
-            return;
-        }
-
-        const filecontent = readFileSync(filename).toString();
-        const xmlContent = JSON.parse(parser.toJson(filecontent));
-
-        if (xmlContent['SimBase.Document'] === undefined || xmlContent['SimBase.Document']['SimConnect.Comm'] === undefined) {
-            parentPort.postMessage({ request: 'LOGERROR', response: 'Invalid SimConnect.xml file found. Trying port 500' });
-            return;
-        }
-
-        const connections = xmlContent['SimBase.Document']['SimConnect.Comm'];
-        let foundValidEntry = false;
-        if (Array.isArray(connections) === true) {
-            connections.every((entry) => {
-                foundValidEntry = this.validateSimConnectConnectionEntry(entry);
-                return foundValidEntry !== true;
-            });
-        } else {
-            foundValidEntry = this.validateSimConnectConnectionEntry(connections);
-        }
-
-        if (foundValidEntry === false) {
-            parentPort.postMessage({ request: 'LOGERROR', response: 'No valid Comm-configuration found. Trying port 500' });
-        }
+            parentPort.postMessage({ request: 'LOGERROR', response: `Connection failed: ${error} - Retry in 10 seconds` });
+            setTimeout(() => this.connectToSim(), 10000);
+        });
     }
 
     constructor() {
-        this.loadSimConnectConfiguration();
         this.connectToSim();
     }
 
     public terminate(): void {
-        this.shutdown = true;
-        if (this.connection !== null) this.connection.close();
-        this.connection = null;
+        this.connection.close();
     }
 
     public sendNavigationDisplayThresholds(thresholdData: NavigationDisplayData): void {
@@ -191,21 +131,14 @@ export class SimConnect {
         const buffer = Buffer.alloc(NavigationDisplayThresholdByteCount);
 
         // fill the buffer
-        buffer.writeUInt16LE(thresholdData.Columns);
-        buffer.writeUInt16LE(thresholdData.Rows);
-        buffer.writeInt16LE(thresholdData.MinimumElevation);
-        buffer.writeUInt8(thresholdData.MinimumElevationMode);
-        buffer.writeInt16LE(thresholdData.MaximumElevation);
-        buffer.writeUInt8(thresholdData.MaximumElevationMode);
+        buffer.writeUInt16LE(thresholdData.Columns, 0);
+        buffer.writeUInt16LE(thresholdData.Rows, 2);
+        buffer.writeInt16LE(thresholdData.MinimumElevation, 4);
+        buffer.writeUInt8(thresholdData.MinimumElevationMode, 6);
+        buffer.writeInt16LE(thresholdData.MaximumElevation, 7);
+        buffer.writeUInt8(thresholdData.MaximumElevationMode, 9);
 
-        this.connection.setClientData(
-            ClientDataId.NavigationDisplayThresholds,
-            ClientDataId.NavigationDisplayThresholds,
-            0,
-            0,
-            NavigationDisplayThresholdByteCount,
-            buffer,
-        );
+        this.thresholdData.setData(ClientDataId.NavigationDisplayThresholds, buffer);
     }
 
     public sendNavigationDisplayTerrainMapFrame(frame: Buffer): void {
@@ -216,13 +149,13 @@ export class SimConnect {
         buffer.writeUInt32LE(frame.byteLength);
         Buffer.concat([buffer, frame]);
 
-        this.connection.setClientData(
+        /* this.connection.setClientData(
             ClientDataId.NavigationDisplayFrame,
             ClientDataId.NavigationDisplayFrame,
             0,
             0,
             buffer.byteLength,
             buffer,
-        );
+        ); */
     }
 }
