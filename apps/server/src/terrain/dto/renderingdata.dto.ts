f58228a1c35c5a393257c1801483b67acf1921d4
Sven Czarnian <devel@svcz.de>
2023-01-10 17:16:40 +0100

    replace unused DTO structures

---
+++ apps/server/src/terrain/dto/renderingdata.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+/* eslint-disable camelcase */
+export class RenderingDataDto {
+    @ApiProperty({ description: 'The rendering timestamp', example: 42 })
+    timestamp: number
+
+    @ApiProperty({ description: 'The encoded pixels', example: '[AHXD45]' })
+    frames: string[]
+}
