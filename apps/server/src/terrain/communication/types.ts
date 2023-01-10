a52a2bb9057e2c201a5d80c2fbdbe3df630e7ca7
Sven Czarnian <devel@svcz.de>
2023-01-10 17:18:43 +0100

    extend the rendering types

--- apps/server/src/terrain/communication/types.ts
+++ apps/server/src/terrain/communication/types.ts
@@ -12,7 +12,8 @@ export interface NavigationDisplay {
     range: number,
     arcMode: boolean,
     active: boolean,
-    brightness: number,
+    mapOffsetX?: number,
+    mapWidth?: number,
     mapHeight?: number,
 }
 
