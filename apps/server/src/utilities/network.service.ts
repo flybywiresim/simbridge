362e1514b251019cd909e7e25debc8a3e98a5aad
Nufflee <19150550+Nufflee@users.noreply.github.com>
2023-01-18 05:58:14 +0100

    fix log spam by ignoring certain DNS decoding errors

--- apps/server/src/utilities/network.service.ts
+++ apps/server/src/utilities/network.service.ts
@@ -38,7 +38,13 @@ export class NetworkService implements OnApplicationShutdown {
         });
 
         this.mDNSServer.on('warning', (error) => {
-            this.logger.warn(`mDNS server warning: ${error.message}`);
+            if (/Cannot decode name \(.+\)/.test(error.message)) {
+                // Ignore this error as it usually comes from irrelevant malformed mDNS packets (particularly from Internet connected radios)
+                // and needlessly spams the log
+                return;
+            }
+
+            this.logger.warn(`mDNS server error: ${error.message}`);
         });
 
         this.mDNSServer.on('ready', () => {
