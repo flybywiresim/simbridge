c82f8afd713f69ca09459069ceb3536eea9b7031
Sven Czarnian <devel@svcz.de>
2023-03-15 22:29:47 +0100

    ensure that the angles handle the wrap around and poles correct

--- apps/server/src/terrain/processing/gpu/elevationmap.ts
+++ apps/server/src/terrain/processing/gpu/elevationmap.ts
@@ -42,12 +42,42 @@ export function createLocalElevationMap(
     bearing = normalizeHeading(bearing + heading);
 
     const projected = projectWgs84(latitude, longitude, bearing, distance);
-    const latStep = (worldMapNortheastLat - worldMapSouthwestLat) / worldMapHeight;
-    const longStep = (worldMapNortheastLong - worldMapSouthwestLong) / worldMapWidth;
+    let latStep = 0.0;
+    if (worldMapSouthwestLat >= latitude) {
+        // we are at the south pole
+        latStep = worldMapSouthwestLat + worldMapNortheastLat + 180.0;
+    } else if (worldMapNortheastLat <= latitude) {
+        // we are at the north pole
+        latStep = 180.0 - worldMapSouthwestLat - worldMapNortheastLat;
+    } else {
+        latStep = worldMapNortheastLat - worldMapSouthwestLat;
+    }
+    latStep /= worldMapHeight;
+
+    // get the longitudinal step and check for 180 deg wrap arounds
+    let longStep = 0.0;
+    if (worldMapNortheastLong < worldMapSouthwestLong) {
+        longStep = 180.0 - worldMapSouthwestLong + Math.abs(worldMapNortheastLong + 180.0);
+    } else {
+        longStep = worldMapNortheastLong - worldMapSouthwestLong;
+    }
+    longStep /= worldMapWidth;
 
     // calculate the pixel movement out of the current position
     const latPixelDelta = (groundTruthLatitude - projected[0]) / latStep;
-    const longPixelDelta = (projected[1] - groundTruthLongitude) / longStep;
+
+    // calculate the pixel delta and check for wrap around situation at 180 deg
+    let longPixelDelta = 0.0;
+    if (Math.abs(projected[1] - groundTruthLongitude) >= 180.0) {
+        if (projected[1] > groundTruthLatitude) {
+            longPixelDelta = 180.0 - projected[1] + Math.abs(groundTruthLongitude) - 180.0;
+        } else {
+            longPixelDelta = 180.0 - groundTruthLongitude + Math.abs(projected[1]) - 180.0;
+        }
+    } else {
+        longPixelDelta = projected[1] - groundTruthLongitude;
+    }
+    longPixelDelta /= longStep;
 
     const y = currentWorldGridY + latPixelDelta;
     const x = currentWorldGridX + longPixelDelta;
