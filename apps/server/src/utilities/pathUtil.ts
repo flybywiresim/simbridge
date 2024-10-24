65d5ab696dfff46b98a81a3fd2a4557eb72ee4dd
Saschl <19493808+Saschl@users.noreply.github.com>
2024-10-24 10:06:20 +0200

    fix: add fallbacks for home folder determination

--- apps/server/src/utilities/pathUtil.ts
+++ apps/server/src/utilities/pathUtil.ts
@@ -1,7 +1,31 @@
 import getPath from 'platform-folders';
 import * as path from 'path';
+import { Logger } from '@nestjs/common';
+import { homedir } from 'os';
+import { execFileSync } from "child_process";
 
-export const getSimbridgeDir = () => path.join(getPath('documents'), 'FlyByWireSim', 'Simbridge');
+export const getSimbridgeDir = () => {
+   try {
+     return path.join(getPath('documents'), 'FlyByWireSim', 'Simbridge');
+   } catch (e) {
+    Logger.warn("Could not get documents path via WinAPI, trying alternate method");
+   }
+   try {
+    const { stdout } = execFileSync("Powershell.exe", [
+      "-Command",
+      `[System.Environment]::GetFolderPath('MyDocuments')`,
+    ]);
+    const path = stdout.trim();
+    if(!path) {
+        throw new Error("Path is empty");
+    }
+    return path;
+  } catch (error) {
+    Logger.warn("Could not get documents path via Powershell, trying to use %USERPROFILE% method");
+  }
+
+  return path.join(homedir(), "Documents");
+}
 
 //@ts-expect-error pkg only defined when running as exe
 export const getExecutablePath = () => (process.pkg ? path.dirname(process.argv[0]) : process.cwd());
