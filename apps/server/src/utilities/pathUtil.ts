07921fd8114d99086be28c991afbafbb47068f4e
Sascha <sascha@dev.com>
2024-10-24 11:24:48 +0200

    format

--- apps/server/src/utilities/pathUtil.ts
+++ apps/server/src/utilities/pathUtil.ts
@@ -2,30 +2,27 @@ import getPath from 'platform-folders';
 import * as path from 'path';
 import { Logger } from '@nestjs/common';
 import { homedir } from 'os';
-import { execFileSync } from "child_process";
+import { execFileSync } from 'child_process';
 
 export const getSimbridgeDir = () => {
-   try {
-     return path.join(getPath('documents'), 'FlyByWireSim', 'Simbridge');
-   } catch (e) {
-    Logger.warn("Could not get documents path via WinAPI, trying alternate method");
-   }
-   try {
-    const { stdout } = execFileSync("Powershell.exe", [
-      "-Command",
-      `[System.Environment]::GetFolderPath('MyDocuments')`,
-    ]);
-    const path = stdout.trim();
-    if(!path) {
-        throw new Error("Path is empty");
+  try {
+    return path.join(getPath('documents'), 'FlyByWireSim', 'Simbridge');
+  } catch (e) {
+    Logger.warn('Could not get documents path via WinAPI, trying alternate method');
+  }
+  try {
+    const output = execFileSync('Powershell.exe', ['-Command', `[System.Environment]::GetFolderPath('MyDocuments')`]);
+    const documents = output.toString().trim();
+    if (!documents) {
+      throw new Error('Path is empty');
     }
-    return path;
+    return path.join(documents, 'FlyByWireSim', 'Simbridge');
   } catch (error) {
-    Logger.warn("Could not get documents path via Powershell, trying to use %USERPROFILE% method");
+    Logger.warn('Could not get documents path via Powershell, trying to use %USERPROFILE% method');
   }
 
-  return path.join(homedir(), "Documents");
-}
+  return path.join(homedir(), 'Documents', 'FlyByWireSim', 'Simbridge');
+};
 
 //@ts-expect-error pkg only defined when running as exe
 export const getExecutablePath = () => (process.pkg ? path.dirname(process.argv[0]) : process.cwd());
