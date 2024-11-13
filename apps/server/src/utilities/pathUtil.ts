08f019527d5ffcb6d9033be5f262984a76787b78
Saschl <19493808+Saschl@users.noreply.github.com>
2024-11-13 21:04:36 +0100

    fix: add folder fallback to prevent app crash (#135)
    If Windows Controlled Folder access is enabled, it will prevent
    simbridge from accessing the documents folder and the app will crash.
    Added a fallback to use the appdata directory, which is not protected by
    default.
    
    Note: the old workaround via powershell was removed, as the reason for
    platform-folders to fail was Windows blocking it as well. So it's not
    needed anymore IMO.
    
    Fixes #134 
    Fixes #133
    Fixes #130
    
    ---------
    
    Co-authored-by: Saschl <noreply@saschl.com>

--- apps/server/src/utilities/pathUtil.ts
+++ apps/server/src/utilities/pathUtil.ts
@@ -1,27 +1,21 @@
 import getPath from 'platform-folders';
 import * as path from 'path';
 import { Logger } from '@nestjs/common';
-import { homedir } from 'os';
-import { execFileSync } from 'child_process';
 
 export const getSimbridgeDir = () => {
   try {
     return path.join(getPath('documents'), 'FlyByWireSim', 'Simbridge');
   } catch (e) {
-    Logger.warn('Could not get documents path via WinAPI, trying alternate method', e);
+    Logger.warn(
+      'Could not get documents path via WinAPI, Windows Controlled Folder Access is likely blocking the app. Using AppData as fallback',
+      e,
+    );
   }
   try {
-    const output = execFileSync('Powershell.exe', ['-Command', `[System.Environment]::GetFolderPath('MyDocuments')`]);
-    const documents = output.toString().trim();
-    if (!documents) {
-      throw new Error('Path is empty');
-    }
-    return path.join(documents, 'FlyByWireSim', 'Simbridge');
+    return path.join(getPath('appdata'), 'FlyByWireSim', 'Simbridge');
   } catch (e) {
-    Logger.warn('Could not get documents path via Powershell, trying to use %USERPROFILE% method', e);
+    Logger.error('Could not get AppData path via WinAPI. Giving up.', e);
   }
-
-  return path.join(homedir(), 'Documents', 'FlyByWireSim', 'Simbridge');
 };
 
 //@ts-expect-error pkg only defined when running as exe
