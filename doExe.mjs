51faeae4ad2fb64c7bc62fc5a95e4dd9b80263a5
Saschl <19493808+Saschl@users.noreply.github.com>
2024-11-14 08:57:54 +0100

    chore: update version to 0.6.2 for exe (#136)

--- doExe.mjs
+++ doExe.mjs
@@ -5,7 +5,7 @@ const build = exe({
   entry: './dist/main.js',
   out: './build/fbw-simbridge.exe',
   pkg: ['-C', 'GZip', '-c', './package.json'], // Specify extra pkg arguments
-  version: '0.6.1',
+  version: '0.6.2',
   target: 'node18-win-x64',
   icon: './apps/server/src/assets/images/tail.ico',
   properties: {
