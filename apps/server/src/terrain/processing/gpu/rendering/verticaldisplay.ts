f5ccb785ea45a3dcd879b2ad8eaf9c9519544f8e
Florian Gross <flogross89@gmail.com>
2025-10-16 02:25:06 +0300

    change VD grey area color

--- apps/server/src/terrain/processing/gpu/rendering/verticaldisplay.ts
+++ apps/server/src/terrain/processing/gpu/rendering/verticaldisplay.ts
@@ -25,7 +25,7 @@ export function renderVerticalDisplay(
   // altitude is above the elevation -> draw the background
   if (altitude > elevation) {
     if (greyBackgroundFromX >= 0 && pixelX >= greyBackgroundFromX) {
-      return [78, 78, 97, 255][colorChannel];
+      return [50, 49, 53, 255][colorChannel];
     } else {
       return [0, 0, 0, 0][colorChannel];
     }
